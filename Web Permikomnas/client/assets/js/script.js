4484832ec2ee2e7b067bf24bc4891ab2ce734f97
SyalomielePratama <129130206+SyalomielePratama@users.noreply.github.com>
2023-10-24 17:30:02 +0700

    Add files via upload

---
+++ Web Permikomnas/client/assets/js/script.js
@@ -0,0 +1,6 @@
+AOS.init({
+    once: true, // Efek hanya dimuat sekali
+    offset: 200, // Menunda efek 200px sebelum elemen muncul
+    duration: 1000, // Durasi animasi 1 detik
+    delay: 0
+  });
